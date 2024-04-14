7a7eb6c8d8d50a1c609d132847dde446c458ff46
vm15717 <vm15717@my.bristol.ac.uk>
2024-04-14 22:01:12 +0100

    first commit. building a simple hypothesis testing library from scratch

---
+++ distributions.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <mathfunc.cpp>
+
+using namespace std;
+double binomialpdf(int, int, double );
+double binomialcdf(int, int, double );
+double normalpdf(double, double, double);
+double normalcdf(double, double, double);
+double bernoullipdf(int, double);
+double bernoullicdf(double, double);
+double poissonpdf(int, double);
+double poissoncdf(int, double);
+
+
+int main(){
+    return 0;
+}
+
+double binomialpdf(int k, int n, double p)
+{
+    return nchoosek(n, k)*pow(p,k)*pow(1-p, n-k);
+}
+
+double normalpdf(double x, double u, double s)
+{
+    return exp(-1*pow(x-u, 2)/(2 * pow(s,2)))*(1/sqrt(2*M_PI*pow(s,2)));
+}
+
+double bernoullicdf(int k, double p)
+{
+    if (p > 1 || p < 0)
+    {
+        throw std::invalid_argument( "the parameter for the bernoulli distribution should be in [0, 1]." );
+    }
+    if (k == 1)
+    {
+        return p;
+    }
+    else if (k == 0)
+    {
+        return 1-p;
+    }
+    else
+    {
+        cout << "Error, k must be an integer either 0 or 1" << endl;
+        return 1;
+    }
+}
+
+double poissonpdf(int x, double l)
+{
+    return exp(-1*l)*pow(l, x)/factorial(x);
+}
+
+double normalcdf(double x, double u, double s)
+{
+    return erf((x-u)/s);
+} 
+
+double poissoncdf(int x, double l)
+{
+    int i = 0;
+    double sum = 0;
+    double s = 1;
+    for (i = 1; i <= x; i++)
+    {
+        s = s*l/i;
+        sum += s;   
+    };
+    return sum*exp(-1*l);
+}
+
+double binomialcdf(int k, int n, double p)
+{
+
+}
+
+double bernoullipdf(int k, double p)
+{
+    if (k == 0)
+    {
+        return p;
+    }
+    else if (k == 1)
+    {
+        return 1-p;
+    }
+}
---
+++ mathfunc.cpp
@@ -0,0 +1,79 @@
+double powerof(double, double);
+double newtonraphsonpow(double, double);
+double exp(double);
+double nchoosek(int, int);
+int factorial(int);
+double cos(double);
+double sin(double);
+double log(double);
+
+
+double newtonraphsonpow(double x, double y)
+{
+    int x = 0;   
+}
+double exp(double x){
+    double s = x;
+    double sum = 0;
+    for (int i = 1; s < -1.0E-14 || s > 1.0E-14; i++)
+    {
+        s = s/i;
+        sum += s;
+    }
+    return sum;
+}
+
+double cos(double x){
+    double s = 1;
+    double sum = 1;
+    for (int i = 1; s < -1.0E-14 || s > 1.0E-14; i++)
+    {
+        s = -s*x*x/((i+1)*i);
+        sum += s;
+    }
+    return sum; 
+}
+
+double sin(double x){
+    double s = x;
+    double sum = x;
+    for (int i = 1; s < -1.0E-14 || s > 1.0E-14; i++)
+    {
+        s = -s*x*x/((2*i+1)*(2*i));
+        sum += s;
+    }
+    return sum; 
+}
+
+
+int factorial(int n){
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        int prod = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            prod = prod* i;
+        }
+        return prod;
+    }
+}
+
+
+
+double nchoosek(int n, int k)
+{
+    double ret = 1;
+    int n1 = n;
+    int k1 = k;
+    for(int i = k; i > 0; i--)
+    {
+        ret = n1/k1 * ret;
+        n1 = n - 1;
+        k1 = k - 1;
+    }
+    return ret;
+}
